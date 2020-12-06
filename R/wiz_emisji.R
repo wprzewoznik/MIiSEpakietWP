fd8599199cbd84a6f9f743926bd364d516b7846c
wprzewoznik <w.przewoznik@gmail.com>
2020-12-06 16:12:16 +0100

    Uzupelnienie potrzebnych odwolan do funkcji

--- R/wiz_emisji.R
+++ R/wiz_emisji.R
@@ -1,34 +1,46 @@
+#' Title
+#'
+#' @param df
+#' @param kat1
+#' @param kat2
+#' @param kategoryczna
+#' @param numeryczna
+#'
+#' @return
+#' @export
+#'
+#' @examples
 wiz_emisji <- function(df = wskazniki,
                 kat1 = Category,
                 kat2 = "Passenger Cars",
                 kategoryczna = Segment,
                 numeryczna = Gamma) {
 
-  kat1 <- enquo(kat1)
-  kategoryczna <- enquo(kategoryczna)
-  numeryczna   <- enquo(numeryczna)
+  kat1 <- rlang::enquo(kat1)
+  kategoryczna <- rlang::enquo(kategoryczna)
+  numeryczna   <- rlang::enquo(numeryczna)
 
   if (!is.null(kat2)) {
     df <- df %>%
-      filter(!!kat1 %in% kat2)
+      dplyr::filter(!!kat1 %in% kat2)
   }
 
   df %>%
-    ggplot(., aes(x = !!kategoryczna,
+    ggplot2::ggplot(., ggplot2::aes(x = !!kategoryczna,
                   y = !!numeryczna,
                   fill = !!kategoryczna)) +
-    geom_boxplot(outlier.shape = NA,
+    ggplot2::geom_boxplot(outlier.shape = NA,
                  col="blue")+
-    theme_minimal() -> p
+    ggplot2::theme_minimal() -> p
 
-  wykres <- p + labs(title = "Zalezosc wartosci parametru emisji
-                     od wybranej kategorii",
-                     subtitle = "Funkcja wizualizacji pakietu",
-                     x = "Wybrana kategoria",
-                     y = "Wartosc wybranego paramteru",
-                     caption = "Opracowano na podstawie danych z EEA",
-                     color = "Category") +
-    ylim(0, 500)
+  wykres <- p + ggplot2::labs(title = "Zalezosc wartosci parametru emisji
+                                      od wybranej kategorii",
+                              subtitle = "Funkcja wizualizacji pakietu",
+                              x = "Wybrana kategoria",
+                              y = "Wartosc wybranego paramteru",
+                              caption = "Opracowano na podstawie danych z EEA",
+                              color = "Category") +
+                          ggplot2::ylim(0, 500)
 
   return(wykres)
 }
