21b0e3a8abbcf71733d2a0dfa039b326ce6682a0
wprzewoznik <w.przewoznik@gmail.com>
2020-12-07 14:24:42 +0100

    Dodanie odwolan do funkcji w wyniku wykorzystania tidyverse

--- R/obl_emisji.R
+++ R/obl_emisji.R
@@ -13,18 +13,18 @@ obl_emisji <- function(dane = input,
                        substancja = c("CO", "EC")) {
 
   out <- wskazniki %>%
-    dplyr::filter(Category %in% kategoria) %>%
-    dplyr::filter(Euro.Standard %in% euro) %>%
-    dplyr::filter(Pollutant %in% substancja) %>%
-    dplyr::filter(Mode %in% mode)
+    dplyr::filter( .data$Category %in% kategoria) %>%
+    dplyr::filter( .data$Euro.Standard %in% euro) %>%
+    dplyr::filter( .data$Pollutant %in% substancja) %>%
+    dplyr::filter( .data$Mode %in% mode)
 
   out <- dplyr::inner_join(x = out, y = input, by = c("Segment","Fuel","Technology"))
 
   out <- out %>%
-    dplyr::mutate(Emisja = Nat * ((Alpha * Procent ^ 2 + Beta * Procent + Gamma + (Delta/Procent))/
-                             (Epsilon * Procent ^ 2 + Zita * Procent + Hta) * (1-Reduction))
+    dplyr::mutate( .data$Emisja = .data$Nat * (( .data$Alpha *  .data$Procent ^ 2 + .data$Beta * .data$Procent + .data$Gamma + ( .data$Delta/ .data$Procent))/
+                             ( .data$Epsilon * .data$Procent ^ 2 + .data$Zita * .data$Procent + .data$Hta) * ( 1- .data$Reduction))
     ) %>%
-    dplyr::select(Category, Fuel, Euro.Standard, Technology, Pollutant, Mode, Segment, Nat, Emisja)
+    dplyr::select( .data$Category,  .data$Fuel, .data$Euro.Standard, .data$Technology, .data$Pollutant, .data$Mode, .data$Segment, .data$Nat, .data$Emisja)
 
   return(out)
 }
