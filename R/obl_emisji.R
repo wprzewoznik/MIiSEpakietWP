e2af3e374865067eca6f273fa337f19e759c2087
wprzewoznik <w.przewoznik@gmail.com>
2020-12-11 16:56:59 +0100

    Uzupelnienie dokumentacji funkcji obl_emisji()

--- R/obl_emisji.R
+++ R/obl_emisji.R
@@ -1,11 +1,24 @@
-#' Obliczanie emisji niskiej - transport
+#' Funkcja do bliczanie emisji pochodzacej z transportu
 #'
-#' @param dane data.frame
-#' @param kategoria char
-#' @param euro char
-#' @param mode vector
-#' @param substancja vector
+#'  Funkcja oblicza emisje na korzystajac z danych zawartych w obiekcie
+#' \code{wskazniki}, a tazke z obiektu \code{input}.
+#' W obiekcie \code{wskazniki} zapisane sa dane o emisji, prygotowane wczesniej.
+#' W obiekcie \code{input} zapisane sa stworzone losowo dane o natezenia ruchu.
+#'
+#' @details Wzor do obliczen emisji:
+#' Emisja = Nat x ((Alpha  x Procent^2 + Beta x Procent + Gamma +
+#' (Delta/Procent)) / (Epsilon x Procent^2 + Zita x Procent + Hta) x (1- Reduction))
+#'
+#' @param dane data.frame - obiekt wejsciowy
+#' @param kategoria char - kategoria pojazdu
+#' @param euro char - norma spalin
+#' @param mode char - tryb jazdy
+#' @param substancja char - mitowane zanieczyszczenie
+#'
 #' @export
+#' @examples
+#' # Obliczenie emisji dla parametrow domyslnych
+#'   tmp <- obl_emisji()
 #'
 obl_emisji <- function(dane = input,
                        kategoria = "Passenger Cars",
