2eb496dba2bb5c38d7545c4739578a92b16b7565
wprzewoznik <w.przewoznik@gmail.com>
2020-12-11 14:51:12 +0100

    Dokumentacja obiektu input

---
+++ R/input.R
@@ -0,0 +1,17 @@
+#' Dane wejsciowe dla funkcji obl_emisji()
+#'
+#' Obiekt jest losowo wygenerowanym wektorem.
+#' Pozostale dane sa wybrane z obiektu wskazniki.
+#'
+#' @format Ramka danych zawiera:
+#' \describe{
+#'  \item{Nat}{Natezenie ruchu}
+#'  \item{Segment}{Rodzaj pojazdow}
+#'  \item{Fuel}{Typ paliwa}
+#'  \item{Technology}{Technologia silnika}
+#' }
+#' @examples
+#' \dontrun{
+#' plikdane
+#' }
+"input"
